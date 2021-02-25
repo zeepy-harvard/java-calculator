978ff409d6cdcb150c58e71fe54fc81a8919ca64
hyunjong_96 <ahajongs@gmail.com>
2021-02-25 18:18:42 +0900

    하버드 java-calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,51 @@
+import java.util.Scanner;
+
 public class Calculator {
     public void Calculator(){};
-    public double sum(double a,double b){
-        return a+b;
+    public int operation(String[] values)throws  Exception{
+        this.validation(values);
+
+        int result=0;
+        for(int i=0;i<values.length-2;i+=2){
+            int firstNumber = Integer.parseInt(values[i]);
+            int secondNumber = Integer.parseInt(values[i+2]);
+            String op = values[i+1];
+            if(i==0){
+                result = operating(firstNumber,secondNumber,op);
+            }else{
+                result = operating(result,secondNumber,op);
+            }
+        }
+        return result;
+    }
+
+    private int operating(int firstNumber,int secondNumber,String op){
+        int result;
+        switch (op){
+            case "+":
+                result = firstNumber+secondNumber;
+                break;
+            case "-":
+                result = firstNumber-secondNumber;
+                break;
+            case "*":
+                result = firstNumber*secondNumber;
+                break;
+            case "/":
+                result = firstNumber/secondNumber;
+                break;
+            default:
+                throw new IllegalStateException("허용되지 않는 연산자: " + op);
+        }
+        return result;
+    }
+
+    public String input(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public void validation(String[] values){
+        if(values.length % 2 == 0) throw new IllegalArgumentException("잘못된 연산입력입니다.");
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,10 +1,26 @@
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
 import org.junit.Test;
 
-public class CalculatorTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+public class CalculatorTest {
     @Test
-    public void testSum(){
-        Calculator c = new Calculator();
-        double result = c.sum(10,50);
+    public void testSum()throws Exception{
+        Calculator calculator = new Calculator();
+
+        String value = "2 + 3 * 4 / 2";
+        String errorValue = "2 + 3 * 4 / ";
+
+        String[] values = value.split(" ");
+        String[] errorValues = errorValue.split(" ");
+
+        assertThat(calculator.operation(values)).isEqualTo(10);
+        assertThatThrownBy(()->{
+            assertThat(calculator.operation(errorValues)).isEqualTo(10);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("잘못된 연산입력입니다.");
     }
 }
