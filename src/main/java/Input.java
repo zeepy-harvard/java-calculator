9e27089e3e877bacfb467c37ab6a0dd1e683c98a
hyunjong_96 <ahajongs@gmail.com>
2021-03-03 16:46:38 +0900

    feat: position change with static, 접근제어자

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Input {
     static private final String SPACE_BLANK =" ";
 
-    static public String inputs(){
+    public static String inputs(){
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
-    static public String[] split(String value){
+    public static String[] split(String value){
         return value.split(SPACE_BLANK);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args){
         Run_Calculator();
     }
-    static private void Run_Calculator(){
+    private static void Run_Calculator(){
         Calculator calculator = new Calculator();
         String[] values = getStringArr(calculator);
         try{
@@ -15,7 +15,7 @@ public class Main {
         }
     }
 
-    static private String[] getStringArr(Calculator calculator){
+    private static String[] getStringArr(Calculator calculator){
         String value = Input.inputs();
         String [] values = Input.split(value);
         return values;
