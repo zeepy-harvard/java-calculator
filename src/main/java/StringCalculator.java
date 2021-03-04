54e4fb68a0a4a92830baa9394cfa3d4cdeb24d12
Yein <flocawint@naver.com>
2021-03-05 01:58:57 +0900

    refactor : main의 간결화
    checkValidation() 메소드 호출을 Operation클래스로 이동
    사람은 식을 입력하고 '=' 버튼만 누른다..

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -4,33 +4,20 @@ public class StringCalculator {
 
     public static void main(String[] args) {
         Operation operation;
+        int result = 0;
 
-        while (true){
+        while (true) {
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
 
-            if(input.equals("-1")){
+            if (input.equals("-1")) {
                 break;
             }
-            operation = new Operation(input);
 
-            start(operation);
+            operation = new Operation();
+            result = operation.runOperation(input);
+            System.out.println("계산 결과 :" + result);
         }
         System.out.println("Exit");
     }
-
-    private static void start(Operation operation) {
-
-        int result = 0;
-
-        try{
-            operation.checkValidation();
-            result = operation.runOperation();
-            System.out.println("계산 결과 :" + result);
-        }catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-    }
-
-
 }
